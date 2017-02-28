f915c398e7a3c7e9ea8519d99cb18cb7b0075007
Federico Squartini <federico.squartini@gmail.com>
2017-02-28 17:20:46 +0100

    Add libraries that wrap the contract

---
+++ VotingSystem/src/main/java/io/positronicbrain/test_0/MetaCoin.java
@@ -0,0 +1,121 @@
+package io.positronicbrain.test_0;
+
+import java.lang.Override;
+import java.lang.String;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Future;
+import org.web3j.abi.EventEncoder;
+import org.web3j.abi.EventValues;
+import org.web3j.abi.TypeReference;
+import org.web3j.abi.datatypes.Address;
+import org.web3j.abi.datatypes.Event;
+import org.web3j.abi.datatypes.Function;
+import org.web3j.abi.datatypes.Type;
+import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.request.EthFilter;
+import org.web3j.protocol.core.methods.response.Log;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.tx.Contract;
+import org.web3j.tx.TransactionManager;
+import rx.Observable;
+import rx.functions.Func1;
+
+/**
+ * <p>Auto generated code.<br>
+ * <strong>Do not modify!</strong><br>
+ * Please use {@link org.web3j.codegen.SolidityFunctionWrapperGenerator} to update.
+ *
+ * <p>Generated with web3j version 2.0.1.
+ */
+public final class MetaCoin extends Contract {
+    private static final String BINARY = "6060604052341561000c57fe5b5b600160a060020a033216600090815260208190526040902061271090555b5b61022f8061003b6000396000f300606060405263ffffffff60e060020a6000350416637bd703e8811461003757806390b98a1114610065578063f8b2cb4f14610098575bfe5b341561003f57fe5b610053600160a060020a03600435166100c6565b60408051918252519081900360200190f35b341561006d57fe5b610084600160a060020a036004351660243561014c565b604080519115158252519081900360200190f35b34156100a057fe5b610053600160a060020a03600435166101e4565b60408051918252519081900360200190f35b600073__ConvertLib.sol:ConvertLib_____________6396e4ee3d6100eb846101e4565b60026000604051602001526040518363ffffffff1660e060020a028152600401808381526020018281526020019250505060206040518083038186803b151561013057fe5b60325a03f4151561013d57fe5b5050604051519150505b919050565b600160a060020a03331660009081526020819052604081205482901015610175575060006101de565b600160a060020a0333811660008181526020818152604080832080548890039055938716808352918490208054870190558351868152935191937fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef929081900390910190a35060015b92915050565b600160a060020a0381166000908152602081905260409020545b9190505600a165627a7a72305820f30a8e61f78d6b10fe09fc792624fd2e8ba12fcf63b5fd58263c3b706961e71f0029";
+
+    private MetaCoin(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        super(contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    private MetaCoin(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        super(contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    public List<TransferEventResponse> getTransferEvents(TransactionReceipt transactionReceipt) {
+        final Event event = new Event("Transfer", 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Address>() {}, new TypeReference<Address>() {}),
+                Arrays.<TypeReference<?>>asList(new TypeReference<Uint256>() {}));
+        List<EventValues> valueList = extractEventParameters(event,transactionReceipt);
+        ArrayList<TransferEventResponse> responses = new ArrayList<TransferEventResponse>(valueList.size());
+        for(EventValues eventValues : valueList) {
+            TransferEventResponse typedResponse = new TransferEventResponse();
+            typedResponse._from = (Address)eventValues.getIndexedValues().get(0);
+            typedResponse._to = (Address)eventValues.getIndexedValues().get(1);
+            typedResponse._value = (Uint256)eventValues.getNonIndexedValues().get(0);
+            responses.add(typedResponse);
+        }
+        return responses;
+    }
+
+    public Observable<TransferEventResponse> transferEventObservable() {
+        final Event event = new Event("Transfer", 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Address>() {}, new TypeReference<Address>() {}),
+                Arrays.<TypeReference<?>>asList(new TypeReference<Uint256>() {}));
+        EthFilter filter = new EthFilter(DefaultBlockParameterName.EARLIEST,DefaultBlockParameterName.LATEST, getContractAddress());
+        filter.addSingleTopic(EventEncoder.encode(event));
+        return web3j.ethLogObservable(filter).map(new Func1<Log, TransferEventResponse>() {
+            @Override
+            public TransferEventResponse call(Log log) {
+                EventValues eventValues = extractEventParameters(event, log);
+                TransferEventResponse typedResponse = new TransferEventResponse();
+                typedResponse._from = (Address)eventValues.getIndexedValues().get(0);
+                typedResponse._to = (Address)eventValues.getIndexedValues().get(1);
+                typedResponse._value = (Uint256)eventValues.getNonIndexedValues().get(0);
+                return typedResponse;
+            }
+        });
+    }
+
+    public Future<TransactionReceipt> getBalanceInEth(Address addr) {
+        Function function = new Function("getBalanceInEth", Arrays.<Type>asList(addr), Collections.<TypeReference<?>>emptyList());
+        return executeTransactionAsync(function);
+    }
+
+    public Future<TransactionReceipt> sendCoin(Address receiver, Uint256 amount) {
+        Function function = new Function("sendCoin", Arrays.<Type>asList(receiver, amount), Collections.<TypeReference<?>>emptyList());
+        return executeTransactionAsync(function);
+    }
+
+    public Future<TransactionReceipt> getBalance(Address addr) {
+        Function function = new Function("getBalance", Arrays.<Type>asList(addr), Collections.<TypeReference<?>>emptyList());
+        return executeTransactionAsync(function);
+    }
+
+    public static Future<MetaCoin> deploy(Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit, BigInteger initialValue) {
+        return deployAsync(MetaCoin.class, web3j, credentials, gasPrice, gasLimit, BINARY, "", initialValue);
+    }
+
+    public static Future<MetaCoin> deploy(Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit, BigInteger initialValue) {
+        return deployAsync(MetaCoin.class, web3j, transactionManager, gasPrice, gasLimit, BINARY, "", initialValue);
+    }
+
+    public static MetaCoin load(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        return new MetaCoin(contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    public static MetaCoin load(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return new MetaCoin(contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    public static class TransferEventResponse {
+        public Address _from;
+
+        public Address _to;
+
+        public Uint256 _value;
+    }
+}
